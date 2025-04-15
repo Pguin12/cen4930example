03915694dbd69c8856939c9a31d73f18819c7fdd
Pguin <pguin341@gmail.com>
2025-04-14 21:06:17 -0400

    random

---
+++ random.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tonuka on 4/14/2025.
+//
+
+#include "random.h"
---
+++ random.h
@@ -0,0 +1,16 @@
+//
+// Created by Tonuka on 4/14/2025.
+//
+
+#ifndef RANDOM_H
+#define RANDOM_H
+
+
+
+class random {
+
+};
+
+
+
+#endif //RANDOM_H
